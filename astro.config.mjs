a511f272243f3601f4c5b09d3915778ce2ac8fd4
Ashkan amjadi <Ashkan.amjadi8412@gmail.com>
2025-09-06 12:28:12 +0330

    deployment ✅👨‍💻

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 
+    site: 'https://ashkanamjadi.github.io',
+    base : '/portfolio',
     integrations: [react(),tailwind()],
     devToolbar : {
       enabled : false
